30efb4ffe401409dd0fd1108229ca96c27002bf9
Saksham Srivastava <saksham16dec@gmail.com>
2024-07-18 17:18:37 +0530

    adding minor changes

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,4 +1,4 @@
-import { Checkbox, Tooltip } from '@mui/material'
+import { Checkbox, Tooltip, Snackbar } from '@mui/material'
 import React, { useState } from 'react'
 import { BiArchive } from 'react-icons/bi'
 import { BsFlagFill } from 'react-icons/bs'
@@ -7,42 +7,60 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { truncateEmail } from '../utils/utils'
 import { removeTicket } from '../redux/slice/ticketSlice'
-
+import { Alert } from '@mui/material'
 
 function Ticket({ ticket }) {
     const [checked, setChecked] = useState(false)
+    const [snackbarOpen, setSnackbarOpen] = useState(false)
     const dispatch = useDispatch()
     const handleCheck = () => {
         setChecked(!checked)
     }
     const handleDelete = () => {
         dispatch(removeTicket(ticket.id))
+        setSnackbarOpen(true)
+    }
+    const handleCloseSnackbar = () => {
+        setSnackbarOpen(false)
     }
     const allChecked = useSelector(state => state.check.allchecked)
     return (
-        <div className='w-full grid m-1  grid-cols-12   items-center bg-white rounded-lg justify-between px-4'>
-            <Checkbox className='col-span-1' checked={checked || allChecked} onChange={handleCheck} />
-            <p className='col-span-3'>{truncateEmail(ticket.email)}</p>
-            <MdMail className='text-xl col-span-1' />
-            <Tooltip title={ticket.status} className='col-span-1'>
-                <div className='w-16 text-center'>
-                    <p className='bg-red-100 cursor-pointer text-red-400 px-1 rounded-md'>{ticket.status}</p>
+        <>
+            <div className='w-full grid m-1  grid-cols-12   items-center bg-white rounded-lg justify-between px-4'>
+                <Checkbox className='col-span-1' checked={checked || allChecked} onChange={handleCheck} />
+                <p className='col-span-3'>{truncateEmail(ticket.email)}</p>
+                <MdMail className='text-xl col-span-1' />
+                <Tooltip title={ticket.status} className='col-span-1'>
+                    <div className='w-16 text-center'>
+                        <p className='bg-red-100 cursor-pointer text-red-400 px-1 rounded-md'>{ticket.status}</p>
+                    </div>
+                </Tooltip>
+                <div className='col-span-2'>
+                    <p>{ticket.subject}</p>
                 </div>
-            </Tooltip>
-            <div className='col-span-2'>
-                <p>{ticket.subject}</p>
+                <Tooltip title="Resolved" className='col-span-1'>
+                    <div className='w-14 text-center'>
+                        <p className='bg-blue-100 cursor-pointer text-blue-400 px-1 rounded-md'>{ticket.flag}</p>
+                    </div>
+                </Tooltip>
+                <BsFlagFill className='text-red-500 col-span-1' />
+                <Link to={`/ticket/${ticket.id}`}>
+                    <BiArchive className='text-lg col-span-1' />
+                </Link>
+                <MdDelete className='col-span-1 cursor-pointer' onClick={handleDelete} />
             </div>
-            <Tooltip title="Resolved" className='col-span-1'>
-                <div className='w-14 text-center'>
-                    <p className='bg-blue-100 cursor-pointer text-blue-400 px-1 rounded-md'>{ticket.flag}</p>
-                </div>
-            </Tooltip>
-            <BsFlagFill className='text-red-500 col-span-1' />
-            <Link to={`/ticket/${ticket.id}`}>
-                <BiArchive className='text-lg col-span-1' />
-            </Link>
-            <MdDelete className='col-span-1 cursor-pointer' onClick={handleDelete} />
-        </div>
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={3000}
+                onClose={handleCloseSnackbar}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+
+            >
+                <Alert onClose={handleCloseSnackbar} severity="success" sx={{ width: '100%' }}>
+                    Ticket deleted successfully!
+                </Alert>
+            </Snackbar>
+        </>
     )
 }
 
